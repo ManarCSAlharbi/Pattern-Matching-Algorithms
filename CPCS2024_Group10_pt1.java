8831a7f0dacb00dcd04782597859317bc83bee0f
Manar <Abdumanar7@gmail.com>
2024-12-24 21:20:31 +0300

    Update CPCS2024_Group10_pt1.java

--- CPCS2024_Group10_pt1.java
+++ CPCS2024_Group10_pt1.java
@@ -1,8 +1,5 @@
 /*
                 CPCS324 - CPCS 324: Algorithms and Data Structures I
-ID: 2206712 | Name: Manar Abdullah Alharbi | Email:  malharbi1414@stu.kau.edu.sa | Section: 03
-ID: 2210455 | Name: Lamar Haitham Fatani   | Email:  lfatani0001@stu.kau.edu.sa  | Section: 03
-ID: 2206375 | Name: Alzahra Abed Alamri    | Email:  aalamri0838@stu.kau.edu.sa  | Section: 03
  ______________________________________________________________________________________________
 Brief Description:
    This program compares the Brute Force and Horspool algorithms for pattern
